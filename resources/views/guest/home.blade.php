f7b63fc605759755dc978f750592b134e31f3ab6
edocico <138378767+edocico@users.noreply.github.com>
2023-12-13 17:09:10 +0100

    created controller and model

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('header-content')
-    
+    <div class="container">
+        <h1 class="text-center">
+            train list
+        </h1>
+    </div>
 @endsection
 
 @section('content')
